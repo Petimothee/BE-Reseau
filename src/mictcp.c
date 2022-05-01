2d0ad39903cd53044285a9310b9c2f0e540b5835
Timothee <timacgarry@Timothees-MacBook-Air.local>
2022-05-01 02:44:48 +0200

    La v4.1 marche en utilisant le mecanisme d'asynchronisme cote serveur. Par contre, la taille du message est de 2 de trop a chaque envoi, ce qui pose un probleme neanmoins important surtout au niveau de l'affichage. Je vais essayer de reparer cela demain.

--- src/mictcp.c
+++ src/mictcp.c
@@ -1,14 +1,18 @@
 #include <mictcp.h>
 #include <api/mictcp_core.h>
+#include <pthread.h>
 #define SIZE 10
 
 mic_tcp_sock sock;
+mic_tcp_sock_addr addr_distant;
 unsigned int PE = 0;
 unsigned int PA = 0; 
 int threshold = 20; //threshold de pertes en pourcentage
 int tabCirc[SIZE];
 static int i=0;
-int start=0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
 
 void error(char* error_message, int line){
     fprintf(stderr, "%s at line %d\n", error_message, line);
@@ -23,7 +27,7 @@ int mic_tcp_socket(start_mode sm)
     int result = -1;
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
     result = initialize_components(sm); /* Appel obligatoire */
-    set_loss_rate(50);
+    set_loss_rate(0);
     sock.fd = 0;
     return sock.fd;
 }
@@ -47,10 +51,52 @@ int mic_tcp_bind(int socket, mic_tcp_sock_addr addr)
  * Met le socket en état d'acceptation de connexions
  * Retourne 0 si succès, -1 si erreur
  */
-int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
+int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr) //cote server, donc asynchrone (-> usage des mutex)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    sock.state = ESTABLISHED;
+    //wait for syn
+    if (sock.fd == socket){
+        sock.state = WAIT_FOR_SYN;
+        if (pthread_mutex_lock(&mutex))
+            error("Erreur lock mutex",__LINE__);
+        
+        while (sock.state==WAIT_FOR_SYN){
+            if (pthread_cond_wait(&cond,&mutex)) //blocage des threads en attendant que la condition soit satisfaite
+                error("Erreur cond_wait SYN",__LINE__);
+        }
+        if (pthread_mutex_unlock(&mutex))
+            error("Erreur unlock mutex",__LINE__);
+    }
+
+    //construction du SYNACK
+    mic_tcp_pdu SYNACK = {0};
+    SYNACK.header.source_port = sock.addr.port;
+    SYNACK.header.dest_port = addr_distant.port;
+    SYNACK.header.ack = 1;
+    SYNACK.header.syn = 1;
+
+    //envoie du SYNACK
+    if(IP_send(SYNACK, *addr) < 0)
+        error("Erreur IP Send", __LINE__);
+    
+    printf("Envoi du SYNACK avec syn = %d et ack = %d\n", SYNACK.header.syn, SYNACK.header.ack);
+
+    //wait for ack
+    if (sock.fd == socket){
+        sock.state = WAIT_FOR_ACK;
+        if (pthread_mutex_lock(&mutex))
+            error("Erreur lock mutex",__LINE__);
+        
+        while (sock.state==WAIT_FOR_ACK){
+            if (pthread_cond_wait(&cond,&mutex)) //blocage des threads en attendant que la condition soit satisfaite
+                error("Erreur cond_wait ACK",__LINE__);
+        }
+        if (pthread_mutex_unlock(&mutex))
+            error("Erreur unlock mutex",__LINE__);
+    }
+    printf("ACK bien recu cote application!\n");
+
+    //sock.state = ESTABLISHED;
     return 0;
 }
 
@@ -61,7 +107,67 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
 int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    sock.state = ESTABLISHED;
+    //sock.state = IDLE;
+    int nb_essais = 0;
+    int nb_max_essais = 10;
+
+    //Construction SYN
+    mic_tcp_pdu SYN = {0};
+    SYN.header.source_port = sock.addr.port;
+    SYN.header.dest_port = addr_distant.port;
+    SYN.header.syn=1;
+    
+    //Envoi du SYN
+    if(IP_send(SYN, addr) < 0)
+        error("Erreur IP Send", __LINE__);
+    nb_essais++;
+
+    //wait for syn ack
+    mic_tcp_pdu SYNACK = {0};
+    int recep_SYNACK = 0;
+    int synack_size;
+    sock.state = WAIT_FOR_SYNACK;
+    while (!recep_SYNACK && (nb_essais < nb_max_essais)) {
+        printf("On rentre dans le wait_for_synack\n");
+        if ((synack_size = IP_recv(&SYNACK, &addr_distant, 100)) < 0) { //timeout
+            //retransmission du SYN
+            if(IP_send(SYN, addr) < 0) 
+                error("error ip-send",__LINE__);
+            sleep(1);
+            nb_essais++;
+        }
+        else {
+            if ((SYNACK.header.syn != 1) || (SYNACK.header.ack != 1)){ //c'est pas un synack
+                printf("\nErreur SYNACK avec syn = %d et ack = %d\n", SYNACK.header.syn, SYNACK.header.ack);
+                if(IP_send(SYN, addr) < 0) 
+                    error("error ip-send",__LINE__);
+                sleep(1);
+                nb_essais++;
+            }
+            else{   //ca marche bien
+                printf("SYNACK bien recu\n");
+                recep_SYNACK = 1;
+                //Construction ACK
+                mic_tcp_pdu ACK = {0};
+                ACK.header.source_port = sock.addr.port;
+                ACK.header.dest_port = addr_distant.port;
+                ACK.header.ack=1;
+                //Envoi de l'ACK
+                if(IP_send(ACK, addr) < 0) 
+                    error("error ip-send",__LINE__);
+
+                printf("ACK bien envoyé avec ack=%d et syn=%d\n", ACK.header.ack, ACK.header.syn);
+                sock.state = ESTABLISHED;
+                break;
+            }
+        }
+    }
+    if((recep_SYNACK == 0) && (nb_essais >= nb_max_essais)){
+        printf("Trop d'essais d'envois du cote client avant de recevoir le synack\n");
+        sock.state = IDLE;
+        return -1;
+    }
+    
     return 0;
 }
 
@@ -75,35 +181,28 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
    
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
     mic_tcp_pdu pdu = {0};
-
+    int nombre_essais = 0;
+    int nombre_essais_max = 10;
     //test si mic_sock = a socket
     if (mic_sock != sock.fd) {
         printf("Erreur socket");
         return -1;
     }
 
-    //header
-    
-    //activation
-
-    //payload
-    char *data = mesg;
-    int size = mesg_size;
-
     //creer PDU
-    pdu.header.source_port=80;
-    pdu.header.dest_port=(unsigned short)mic_sock;
+    pdu.header.source_port=sock.addr.port; 
+    pdu.header.dest_port=addr_distant.port; 
     pdu.header.seq_num=PE;
-    pdu.payload.data = data;
-    pdu.payload.size = size;
+    pdu.payload.data=mesg;
+    pdu.payload.size=mesg_size;
 
     //envoi
     int sent_size = 0;
-    if ((sent_size = IP_send(pdu, sock.addr)) == -1) {
-        printf("Erreur IP_send");
-        return(-1);
+    if ((sent_size = IP_send(pdu, addr_distant)) == -1) {
+        error("Erreur IP_send", __LINE__);
     }
     PE = (PE+1)%2;
+    nombre_essais++;
 
     //on va creer une liste circulaire avec un 1 ou un 0 dans chaque case. 1 si perte, 0 sinon. S'il y a plus de 1 que le threshold (variable globale) dans le tableau circulaire, on renvoie le dernier paquet
     //gestion de fiabilite partielle
@@ -113,35 +212,35 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
     mic_tcp_pdu ack = {0}; 
     //gestion de fiabilite partielle
     int compteur1=0;
-    while (active) {
+    while (active && (nombre_essais < nombre_essais_max)) {
         printf("On rentre dans le wait_for_ack\n");
-        if ((ack1 = IP_recv(&ack, &sock.addr, 100)) < 0) { //timeout
+        if ((ack1 = IP_recv(&ack, &addr_distant, 100)) < 0) { //timeout
             tabCirc[i] = 1;
-            i=(i+1)%SIZE; //on reinitialize l'indice a 0 si il depasse la taille du tableau (liste circulaire version sans pointeurs)
-            //on parcourt le tableau pour voir si on doit renvoyer ou non le paquet
-            for(int j=0; j<SIZE; j++){
+            for(int j=0; j<SIZE; j++){            //on parcourt le tableau pour voir si on doit renvoyer ou non le paquet
                 compteur1 = compteur1 + tabCirc[j];
             }
-            if(compteur1>(threshold/100)*size){
-                if(IP_send(pdu, sock.addr) < 0) // à intégrer un nombre max de retransmission  
+            if(compteur1>(threshold/100)*SIZE){     //s'il y a plus d'erreur que le threshold, on renvoi le paquet
+                if(IP_send(pdu, addr_distant) < 0)  
                     error("error ip-send",__LINE__);
+                nombre_essais++;
             }
+            i=(i+1)%SIZE; //on reinitialize l'indice a 0 s'il depasse la taille du tableau (liste circulaire version sans pointeurs)
         }
         else { //réception ack
             if ((ack.header.ack_num != PE) || (ack.header.ack != 1)){ //c'est pas le bon
                 printf("\nErreur pdu->header.ack_num != PE\n");
                 tabCirc[i] = 1;
-                i=(i+1)%SIZE;
-                //on parcourt le tableau pour voir si on doit renvoyer ou non le paquet
                 for(int k=0; k<SIZE; k++){
                     compteur1 = compteur1 + tabCirc[k];
                 }
                 if(compteur1>(threshold/100)*SIZE){
-                    if(IP_send(pdu, sock.addr) < 0) // à intégrer un nombre max de retransmission  
+                    if(IP_send(pdu, addr_distant) < 0) 
                         error("error ip-send",__LINE__);
+                    nombre_essais++;
                 }
+                i=(i+1)%SIZE;
             }
-            else{   //ca marche bien (askip)
+            else{   //ca marche bien
                 printf("Accuse de reception bien recu\n");
                 tabCirc[i] = 0;
                 i=(i+1)%SIZE;                //pas besoin de parcourir le tableau pq on a pas d'erreur sur i
@@ -149,14 +248,11 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
                 break;
             }
         }
-        
-        
     }
 
     if (ack1<0)
         error("erreur ack", __LINE__);
     
-    //PE = (PE+1)%2;
     printf("seq num : %d\n", pdu.header.seq_num);
     return sent_size;
 }
@@ -170,19 +266,13 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 int mic_tcp_recv (int socket, char* mesg, int max_mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    /*
-    mic_tcp_payload p;
-    p.data= malloc(max_mesg_size);
-    p.size = app_buffer_get(p);
-    memcpy(mesg, p.data, max_mesg_size);
-    free(p.data);
-    return p.size; */
-       mic_tcp_payload app_buff = {
+
+    mic_tcp_payload app_buff = {
         .data = mesg,
         .size = max_mesg_size
     };
-    //app_buff.data = mesg;
-    //app_buff.size = max_mesg_size;
+    
+    //on recupere du buffer
     return app_buffer_get(app_buff);
 }
 
@@ -205,29 +295,53 @@ int mic_tcp_close (int socket)
  */
 void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr){
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    //desencapsuler PDU
-    /*mic_tcp_header header_pdu = pdu.header;
-    mic_tcp_payload payload_pdu = pdu.paypload ;     
-    char* msg= payload_pdu.data;*/
-    // (on insere le pdu dans le buffer de réception car on est en  asynchrone coté réception)
-   
-    printf("pdu seq num %d \n", pdu.header.seq_num); 
-    mic_tcp_pdu ack; 
-    ack.header.ack_num = PA; 
-    ack.header.ack = 1; 
-    ack.payload.size = 0; 
-    ack.payload.data = NULL;
-    if (pdu.header.seq_num == PA) {
-        printf("seq num == PA\n");
-        app_buffer_put(pdu.payload);
-        PA = (PA +1) % 2; 
-        ack.header.ack_num = PA; 
+    //il faut maintenant rajouter la recuperation du syn et de l'ack du cote serveur car il est asynchrone. Ainsi on va utiliser la variable de condition et broadcast quand on recoit un pdu avec syn=1 et ack=1 respectivement, ce qui enleve le blocage de threads imposé dans l'accept().
+
+    //recuperation du syn
+    if(sock.state == WAIT_FOR_SYN){
+        if((pdu.header.syn == 1) && (pdu.header.ack != 1)){
+            if(pthread_cond_broadcast(&cond) != 0)
+                error("Erreur broadcast cond", __LINE__);
+            sock.state = WAIT_FOR_ACK;
+            if(pthread_mutex_unlock(&mutex) != 0)
+                error("Erreur mutex unlock", __LINE__);
+        }
     }
-    if(IP_send(ack, addr) < 0)
-        error("problem ack", __LINE__);
+    //recuperation de l'ack
+    else if(sock.state == WAIT_FOR_ACK){
+        if((pdu.header.ack == 1) && (pdu.header.syn != 1)){
+            if(pthread_cond_broadcast(&cond) != 0)
+                error("Erreur broadcast cond", __LINE__);
+            sock.state = ESTABLISHED; //wait_for_ack est la derniere etape avant d'etablir la connexion
+            if(pthread_mutex_unlock(&mutex) != 0)
+                error("Erreur mutex unlock", __LINE__);
+            printf("ACK bien recu cote serveur!\n");
+
+        }
+    }
+
+    //recuperation de data
+    else{
+        printf("seq num : %d \n", pdu.header.seq_num); 
+        
+        //insertion dans buffer si son seq num == PA
+        if (pdu.header.seq_num == PA) {
+            printf("seq num == PA\n");
+            app_buffer_put(pdu.payload);
+            //maj du PA
+            PA = (PA +1) % 2; 
+            //construction ack
+            mic_tcp_pdu ack={0}; 
+            ack.header.source_port = sock.addr.port;
+            ack.header.dest_port = addr.port;
+            ack.header.ack_num = PA; 
+            ack.header.ack = 1; 
+            ack.header.ack_num = PA; 
+            //envoi de l'ack
+            if(IP_send(ack, addr) < 0)
+                error("problem ack", __LINE__);
+        }
+    }
 }
 
-
-//gbenalay@insa-toulouse.fr
-//ghada gharbi
 //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
